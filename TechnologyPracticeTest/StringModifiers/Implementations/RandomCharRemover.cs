f8905bf53d1a94cc58dd1cd7486f519a89cf47ab
antspring <max.verzilov32@gmail.com>
2025-08-23 02:30:54 +0500

    feat: added random char remover

---
+++ TechnologyPracticeTest/StringModifiers/Implementations/RandomCharRemover.cs
@@ -0,0 +1,40 @@
+using System.Text.Json;
+using TechnologyPracticeTest.StringModifiers.Interfaces;
+
+namespace TechnologyPracticeTest.StringModifiers.Implementations;
+
+public class RandomCharRemover : IStringModifier
+{
+    public string Execute(string input)
+    {
+        var randomIndex = GetRandomIndexAsync(input.Length).GetAwaiter().GetResult();
+
+        return input.Remove(randomIndex, 1);
+    }
+
+    private async Task<int> GetRandomIndexAsync(int stringLength)
+    {
+        using var client = new HttpClient { BaseAddress = new Uri("http://www.randomnumberapi.com/api/v1.0") };
+
+        var response = await client.GetAsync($"/api/v1.0/randomnumber?max={stringLength - 1}");
+
+        if (response.IsSuccessStatusCode)
+        {
+            return await ParseIndexFromResponse(response);
+        }
+
+        return GetRandomIndex(stringLength);
+    }
+
+    private async Task<int> ParseIndexFromResponse(HttpResponseMessage response)
+    {
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<int[]>(content)[0];
+    }
+
+    private int GetRandomIndex(int stringLength)
+    {
+        var random = new Random();
+        return random.Next(stringLength);
+    }
+}
