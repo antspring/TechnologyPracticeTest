76087923a3ecc6335cce6f42dcaca33528826e42
antspring <max.verzilov32@gmail.com>
2025-08-23 18:09:42 +0500

    feat: override IsValid method

--- TechnologyPracticeTestWeb/Validators/Implementations/EnglishAlphabetValidator.cs
+++ TechnologyPracticeTestWeb/Validators/Implementations/EnglishAlphabetValidator.cs
@@ -6,6 +6,19 @@ namespace TechnologyPracticeTestWeb.Validators.Implementations;
 
 public class EnglishAlphabetValidator : ValidationAttribute, IStringValidator
 {
+    protected override ValidationResult IsValid(object input, ValidationContext context)
+    {
+        try
+        {
+            Validate(input as string);
+            return ValidationResult.Success;
+        }
+        catch (StringValidationException e)
+        {
+            return new ValidationResult(e.Message);
+        }
+    }
+
     public void Validate(string input)
     {
         var invalidCharacters = input.Where(c => !(c >= 'a' && c <= 'z')).ToArray();
