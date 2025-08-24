799806869d94d0cb77b74dfb9a5b4d69d7bffca8
antspring <max.verzilov32@gmail.com>
2025-08-25 01:07:38 +0500

    feat: added new project

---
+++ TechnologyPracticeUnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TechnologyPracticeUnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
